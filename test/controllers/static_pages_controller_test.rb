d569fbc92749519d3b2005bb7423c77d9295f57a
Nikita Tikhonov <tikhonnikita987@gmail.com>
2018-11-27 15:13:39 +0400

    Make basic user model

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -8,24 +8,26 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get root_url
     assert_response :success
-    assert_select "title", "Home page | #{@base_title}"
+    assert_select "title", full_title("Home page")
+    # assert_select "title", "Home page | #{@base_title}"
   end
 
   test "should get help" do
     get help_url
     assert_response :success
-    assert_select "title", "Help page | #{@base_title}"
+    assert_select "title", full_title("Help page")
   end
 
   test "should get about" do
     get about_path
     assert_response :success
-    assert_select "title", "About page | #{@base_title}"
+    assert_select "title", full_title("About page")
   end
 
   test "should get contact" do
     get contact_path
     assert_response :success
-    assert_select "title", "Contact page | #{@base_title}"
+    assert_select "title", full_title("Contact page")
   end
+
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,19 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get users_new_url
-    assert_response :success
+  def setup
+    @base_title = "Ruby on Rails Tutorial SampleApp"
   end
 
+  test "should get sign up" do
+    get signup_path
+    assert_response :success
+    assert_select "title", full_title("Sign up")
+  end
+  test 'should get signup' do
+    get signup_path
+    assert_response :success
+    assert_select "title", "Sign up | #{@base_title}"
+  end
 end
+
