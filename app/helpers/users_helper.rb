487095bf95481eaeeb92db1543f8c4e10246e3b7
Semyen Pukhov <sspukhov@yandex.ru>
2018-12-24 01:43:25 +0300

    Finish user signup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+	# Возвращает граватар для данного пользователя.
+	def gravatar_for(user)
+		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+		gravatar_id = "205e460b479e2e5b48aec07710c08d50.jpg"
+		gravatar_url = "https://www.gravatar.com/avatar/#{gravatar_id}"
+		image_tag(gravatar_url, alt: user.name, class: "gravatar")
+	end
 end
